ea9073a82702359a14cd622537bdd9e3d413f6ed
Kristea Gamel <krgamel@gmail.com>
2019-02-02 12:20:33 -0600

    aded two events to nav
    Signed-off-by: Kristea Gamel <krgamel@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+let mainNav = document.querySelector('.main-navigation');
+mainNav.addEventListener('mouseenter', (event) => {
+    mainNav.style.backgroundColor = 'teal';
+})
+
+let logoHead = document.querySelector('.logo-heading');
+logoHead.addEventListener('click', (event) => {
+    alert('Are you ready for FUN?!')
+})
+
+let introImg = document.getElementById("img[src='fun-bus.jpg']");
+introImg.addEventListener('mouseover', (event) => {
+    introImg.src = "img/yellow-bus.jpg"
+})
